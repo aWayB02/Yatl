0bdcf36db7dfb141d2f3178cd7e89e4d16e0b4d4
khabib shahbanov <khabibtime@rambler.ru>
2026-03-05 00:43:25 +0300

    add ref_plan, img

--- src/yatl/request.py
+++
@@ -1,7 +0,0 @@
-class Request:
-    def __init__(self, url, method, headers, data, timeout):
-        self.url = url
-        self.method = method
-        self.headers = headers
-        self.data = data
-        self.timeout = timeout
